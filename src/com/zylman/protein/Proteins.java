0c7072e50421b117efada1d32aff3a0823b70d8e
Alex Zylman <alex@zylman.com>
2011-11-27 22:37:44 -0600

    create our protein map

--- src/com/zylman/protein/Proteins.java
+++ src/com/zylman/protein/Proteins.java
@@ -4,12 +4,12 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Proteins {
 	
-	List<FeatureVector> featureVectors = new ArrayList<FeatureVector>();
+	Map<String, FeatureVector> featureVectors = new HashMap<String, FeatureVector>();
 	
 	Proteins(String filePath) throws FileNotFoundException, IOException {
 		BufferedReader br = new BufferedReader(new FileReader(filePath));
@@ -17,7 +17,9 @@ public class Proteins {
 		//String line = br.readLine(); // Remove column headings
 		String line;
 		while ((line = br.readLine()) != null) {
-			System.out.println(line);
+			String[] id = line.split(">dip:");
+			String sequence = br.readLine();
+			featureVectors.put(id[0], new FeatureVector(sequence));
 		}
 	}
 }
