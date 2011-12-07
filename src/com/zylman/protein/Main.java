35c840f33d205e4529cb4d6c3ac9827b90593531
Alex Zylman <alex@zylman.com>
2011-12-06 20:02:27 -0600

    break up our data into separate training and testing sets

--- src/com/zylman/protein/Main.java
+++ src/com/zylman/protein/Main.java
@@ -3,6 +3,7 @@ package com.zylman.protein;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -49,14 +50,39 @@ public class Main {
 			endTime = new Date().getTime() / 1000;
 			System.out.println("Retrieved proteins in " + (endTime - startTime) + " seconds");
 			
-			BufferedWriter out = new BufferedWriter(new FileWriter("out.txt"));
-			for (Interaction interaction : interactions) {
-				out.write(interaction.toString(proteins));
-			}
+			List<Interaction> trainingSet = new ArrayList<Interaction>();
+			List<Interaction> testingSet = new ArrayList<Interaction>();
+			splitUpDataset(interactions, trainingSet, testingSet, .3, .3);
+			
+			writeInteractionsToFile(trainingSet, proteins, "out.data");
 		} catch (IOException ex) {
 			System.out.println("IOException: " + ex.getMessage());
 		} catch (ProteinException ex) {
 			System.out.println("ProteinException: " + ex.getMessage());
+		}
+	}
+	
+	private static void splitUpDataset(List<Interaction> interactions, List<Interaction> trainingSet, List<Interaction> testingSet, double trainingRatio, double testingRatio) {
+		int cur = 0;
+		int trainingBreakPoint = (int)((double)interactions.size() * trainingRatio);
+		int testingBreakPoint = (int)((double)interactions.size() * testingRatio) + trainingBreakPoint;
+		for (Interaction interaction : interactions) {
+			if (cur > testingBreakPoint) {
+				break;
+			} else if (cur > trainingBreakPoint) {
+				testingSet.add(interaction);
+			} else {
+				trainingSet.add(interaction);
+			}
+			cur++;
+		}
+	}
+	
+	private static void writeInteractionsToFile(List<Interaction> interactions, Map<String, FeatureVector> proteins, String fileName) throws IOException {
+		BufferedWriter out = new BufferedWriter(new FileWriter("out.txt"));
+		for (Interaction interaction : interactions) {
+			out.write(interaction.toString(proteins));
+			out.write("\n");
 		}
 	}
 }
